859dccd2c02e7ca2cbaee64f0ace7c334ba3266c
WalkWeb <av18775@list.ru>
2019-06-07 20:29:39 +0300

    Add decorator pattern

--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
+use Patterns\Decorator\Product;
+use Patterns\Decorator\PartnerProductDecorator;
 
 
+$product = new Product(100, 200);
+$partnerProduct = new PartnerProductDecorator($product, 10, -20);
 
+var_dump($partnerProduct->getBuyPrice());  // 110
+var_dump($partnerProduct->getSellPrice()); // 160
